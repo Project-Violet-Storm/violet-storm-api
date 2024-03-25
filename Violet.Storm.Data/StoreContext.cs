3da9595c773eb532f9cb8e0128bfed5dd98e796e
Luke-Tuttle <tuttle.245@osu.edu>
2024-03-24 20:23:52 -0400

    Created Database Context

---
+++ Violet.Storm.Data/StoreContext.cs
@@ -0,0 +1,14 @@
+﻿using Violet.Storm.Domain.Catalog;
+using Microsoft.EntityFrameworkCore;
+
+namespace Violet.Storm.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Item> Items {get; set; }    
+    }
+}    
